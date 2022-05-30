58c8fcfbec40100e8e421e85403ec5b685412ffd
Oleg <Olegin@yandex.ru>
2022-05-30 20:25:36 +0300

    3

---
+++ ex03/Account.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include "Account.hpp"
+
+int main()
+{
+	Account td(100);
+	td.displayAccountsInfos();
+	Account tf(192);
+	tf.displayAccountsInfos();
+	Account tr(3892);
+	tr.displayAccountsInfos();
+	return (0);
+}
