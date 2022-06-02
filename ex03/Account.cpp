3a30d6a13650d769d6193a40e5e7704791b216c0
Oleg <Olegin@yandex.ru>
2022-06-02 21:04:57 +0300

    5

--- ex03/Account.cpp
+++ ex03/Account.cpp
@@ -7,20 +7,22 @@
 	int Account :: _totalAmount = 0;
 	int Account :: _totalNbDeposits = 0;
 	int Account :: _totalNbWithdrawals = 0;
+	
 	Account::~Account(){}
-	int	Account::getNbAccounts( void )
+
+	int		Account::getNbAccounts( void )
 	{
 		return (_nbAccounts);
 	}
-	int	Account::getTotalAmount( void )
+	int		Account::getTotalAmount( void )
 	{
 		return(_totalAmount);
 	}
-	int	Account::getNbDeposits( void )
+	int		Account::getNbDeposits( void )
 	{
 		return (_totalNbDeposits);
 	}
-	int	Account::getNbWithdrawals( void )
+	int		Account::getNbWithdrawals( void )
 	{
 		return (_totalNbWithdrawals);
 	}
@@ -42,14 +44,13 @@
 		Account::_displayTimestamp();
 		std::cout << "index:" << _accountIndex << ";";
 		std::cout << "amount:" << _amount << ";";
+		std::cout << "p_amount:" << _accountIndex << ";";
 		std::cout << "created:" << std::endl;
 		_nbDeposits = 0;
 		_nbWithdrawals = 0;
 	}
 
-Account::Account(){
-
-}
+	Account::Account(){}
 void Account::_displayTimestamp(void)
 {
 	std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
@@ -74,6 +75,16 @@ void	Account::displayStatus( void ) const
 		std::cout << "withdrawals:" << _nbWithdrawals << ";" << std::endl;
 }
 
+/*void	makeDeposit( int deposit )
+{
+	Account::_displayTimestamp();
+		std::cout << "index:" << _accountIndex << ";";
+		std::cout << "p_amount:" << _amount << ";";
+		std::cout << "deposit:" << _nbDeposits << ";";
+		std::cout << "amount:" << _amount << ";";
+		std::cout << "withdrawals:" << _nbWithdrawals << ";" << std::endl;
+}*/
+
 int main()
 {
 
@@ -94,6 +105,15 @@ int main()
 	f_w5.displayStatus();
 	f_w6.displayStatus();
 	f_w7.displayStatus();
+	Account::displayAccountsInfos();
+	/*Account f_w(5);
+	Account f_w1(765);
+	Account f_w2(564);
+	Account f_w3(2);
+	Account f_w4(87);
+	Account f_w5(23);
+	Account f_w6(9);
+	Account f_w7(20);*/
 
 	return (0);
 }
